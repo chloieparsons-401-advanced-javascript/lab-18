dc859dbdf989e4057a68688461e2e3716789957a
Chloie Parsons <chloieparsons@gmail.com>
2019-06-19 19:59:41 -0700

    Created test files

---
+++ __tests__/app.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// app - Write tests around all of your units
+// File Read, File Save, Uppercase
+// Mock the fs module methods so that your tests don’t use actual files
+
+// I have NEVER EVER mocked fs module methods. I read the documentation on Jest and am a little confused by this. I will ask Adam about it tomorrow.
---
+++ __tests__/logger.test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// logger - Test event handler functions (not event listeners themselves)
+// Use spies to help testing your logger methods (assert that console.log was called right)
